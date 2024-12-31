5ceabda7d42afaf125e0fe120e10da94a1ef5bab
Syeda Nameera Azam <163463330+nameeraazam@users.noreply.github.com>
2024-12-31 17:36:00 +0500

    Update BestSelling.tsx

--- src/components/BestSelling.tsx
+++ src/components/BestSelling.tsx
@@ -1,14 +1,20 @@
 import Link from "next/link";
 import ProductsCard from "./ProductsCard";
-import BS1 from '../../public/images/1 (1).png';
-import BS2 from '../../public/images/2 (1).png';
-import BS3 from '../../public/images/3 (1).png';
-import BS4 from '../../public/images/4.png';
+
+type Product = {
+  image: string;
+  productAlt: string;
+  title: string;
+  price: number;
+  cutPrice: number;
+  reviewStars: number;
+  reviewCount: number;
+}
 
 function BestSelling() {
-  const products = [
+  const products: Product[] = [
     {
-      image: BS1,
+      image: "/images/bestselling1.png",
       productAlt: "coat",
       title: "The North Coat",
       price: 260,
@@ -17,7 +23,7 @@ function BestSelling() {
       reviewCount: 65
     },
     {
-      image: BS2,
+      image: "/images/bestselling2.png",
       productAlt: "bag",
       title: "Gucci duffle bag",
       price: 960,
@@ -26,7 +32,7 @@ function BestSelling() {
       reviewCount: 65
     },
     {
-      image: BS3,
+      image: "/images/bestselling3.png",
       productAlt: "cpu cooler",
       title: "RGB Liquid CPU Cooler",
       price: 160,
@@ -35,7 +41,7 @@ function BestSelling() {
       reviewCount: 65
     },
     {
-      image: BS4,
+      image: "/images/bestselling4.png",
       productAlt: "book shelf",
       title: "Small BookShelf",
       price: 360,
@@ -58,8 +64,8 @@ function BestSelling() {
             Best Selling Products
           </h1>
         </div>
-        <Link 
-          href="/products" 
+        <Link
+          href="/products"
           className="py-3 px-6 sm:py-3 sm:px-12 bg-[#db4444] text-white rounded-sm text-sm sm:text-base hover:bg-[#c13e3e] transition-colors"
         >
           View All
